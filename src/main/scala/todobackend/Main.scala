9a5451a13944a60a801869a6aaa673c5e706b26c
Stefan Holzmüller <stefanholzmueller@github>
2020-08-13 14:46:50 +0200

    CORS

--- src/main/scala/todobackend/Main.scala
+++ src/main/scala/todobackend/Main.scala
@@ -3,14 +3,15 @@ package todobackend
 import java.util.concurrent.Executors
 
 import cats.effect.{Blocker, ExitCode, IO, IOApp}
-import org.http4s.{Headers, HttpRoutes, MediaType, Response}
+import cats.implicits._
 import org.http4s.dsl.io._
 import org.http4s.headers._
-import org.http4s.implicits.{http4sKleisliResponseSyntaxOptionT, _}
-import org.http4s.server.Router
+import org.http4s.implicits._
 import org.http4s.server.blaze.BlazeServerBuilder
+import org.http4s.server.middleware.CORS
 import org.http4s.server.staticcontent.WebjarService.Config
 import org.http4s.server.staticcontent.webjarService
+import org.http4s.{HttpRoutes, MediaType}
 
 import scala.concurrent.ExecutionContext.global
 
@@ -18,10 +19,15 @@ object Main extends IOApp {
 
   private val todoServer = new TodoServer
 
+  private val corsRoutes = HttpRoutes.of[IO] {
+    case OPTIONS -> _ =>
+      Ok()
+  }
+  private val todoRoutes = CORS(
+    todoServer.routes
+  )
   private val webjarRoutes: HttpRoutes[IO] = webjarService(
-    Config(
-      blocker = Blocker.liftExecutorService(Executors.newFixedThreadPool(4))
-    )
+    Config(Blocker.liftExecutorService(Executors.newFixedThreadPool(4)))
   )
   private val documentationRoutes = HttpRoutes.of[IO] {
     case GET -> Root =>
@@ -29,11 +35,7 @@ object Main extends IOApp {
     case GET -> Root / "openapi.json" =>
       Ok(TodoDocumentation.apiJson, `Content-Type`(MediaType.application.json))
   }
-  private val httpApp = Router(
-    "/" -> todoServer.routes,
-    "/" -> webjarRoutes,
-    "/" -> documentationRoutes
-  ).orNotFound
+  private val httpApp = (corsRoutes <+> todoRoutes <+> webjarRoutes <+> documentationRoutes).orNotFound
 
   def run(args: List[String]): IO[ExitCode] =
     BlazeServerBuilder[IO](global)
